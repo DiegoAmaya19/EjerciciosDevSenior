f0def56082ed56b6b9e7f694d03d2f98730f47fb
DiegoAmaya19 <141657295+DiegoAmaya19@users.noreply.github.com>
2025-10-22 16:31:12 -0500

    Impl de servicio de SumaService

--- EjerciciosModulo07SpringSecurity/IJM7-http-responses-main/src/main/java/com/devsenior/cdiaz/http/responses/Service/DefaultService.java
+++ EjerciciosModulo07SpringSecurity/IJM7-http-responses-main/src/main/java/com/devsenior/cdiaz/http/responses/Service/DefaultService.java
@@ -1,14 +1,16 @@
 package com.devsenior.cdiaz.http.responses.Service;
 
+import org.springframework.stereotype.Service;
 import com.devsenior.cdiaz.http.responses.dto.SumaResponse;
 
+@Service
 public class DefaultService {
-    public SumaResponse suma(Integer num1, Integer num2) {
+    public SumaResponse suma(String num1, String num2) {
         var responseBuilder = SumaResponse.builder();
 
         responseBuilder
                 .isError(false)
-                .value(num1 + num2);
+                .value(Integer.valueOf(num1) + Integer.valueOf(num2));
 
         return responseBuilder.build();
     }
---
+++ EjerciciosModulo07SpringSecurity/IJM7-http-responses-main/src/main/java/com/devsenior/cdiaz/http/responses/controller/DefaultExceptionController.java
@@ -0,0 +1,30 @@
+package com.devsenior.cdiaz.http.responses.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsenior.cdiaz.http.responses.Service.DefaultService;
+import com.devsenior.cdiaz.http.responses.dto.SumaResponse;
+
+@RestController
+@RequestMapping("/api/exception")
+public class DefaultExceptionController {
+
+    private final DefaultService defaultService;
+    
+    public DefaultExceptionController(DefaultService defaultService) {
+        this.defaultService = defaultService;
+    }
+
+    @GetMapping("/suma/{num1}/{num2}")
+    public SumaResponse getSuma(@PathVariable String num1, @PathVariable String num2) {
+       
+        return defaultService.suma(num1, num2);
+
+    }
+
+}
